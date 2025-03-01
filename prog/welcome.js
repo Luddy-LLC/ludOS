0467d627bd0fb81eb1d42ce059144524845f5765
NouveauReece <nouveaureece@protonmail.com>
2025-03-01 01:07:34 -0500

    Image fix?

--- prog/welcome.js
+++ prog/welcome.js
@@ -1,7 +1,7 @@
 class WelcomeProgram extends Program {
     createWindow() {
         let body = `
-        <img src="unlogo.png" width="20%" style="filter: invert();margin: 0 40%;padding: 10px 0px;">
+        <img src="./unlogo.png" width="20%" style="filter: invert();margin: 0 40%;padding: 10px 0px;">
         <p>Welcome, <strong>Representative of ${name}</strong>!<br><br>
         Your Windows 98 account has been provisioned, so you now have secure access to view communications from your home government or council. To view these messages, visit the <strong>Message Center</strong>. You are also welcome to take notes in <strong>Notepad</strong>, which will securely delete your notes when closed.<br><br>
         Please let us know you have any questions, and enjoy your new account.<br></p>
@@ -10,7 +10,7 @@ class WelcomeProgram extends Program {
         let wminfo = {
             title: 'Welcome to your Windows 98 Account!',
             name:  'Welcome!',
-            icon:  'img/desktop/MyDocuments.png',
+            icon:  'ludOS/img/desktop/MyDocuments.png',
         }
 
         return [wminfo, body]
